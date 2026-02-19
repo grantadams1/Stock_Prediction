d37b25b1d45a7a883874529a5db529abad6f97a5
grantadams1 <grant.adams@tcu.edu>
2026-02-19 17:42:44 -0600

    Refactor imports and update model keys in Streamlit app

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -59,8 +59,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer.shap',
         "pipeline": 'finalized_model.tar.gz',
-        "keys": ["MSFT", "IBM", "GOOGL", "DEXJPUS", "SP500", "VIXCLS"],
-        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["MSFT", "IBM", "GOOGL", "DEXJPUS", "SP500", "VIXCLS"]]
+        "keys": ["IBM", "GOOGL", "DEXJPUS", "SP500", "VIXCLS"],
+        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["IBM", "GOOGL", "DEXJPUS", "SP500", "VIXCLS"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -151,6 +151,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
